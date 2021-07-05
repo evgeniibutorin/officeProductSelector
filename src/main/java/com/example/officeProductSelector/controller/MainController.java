d2d74c9b4c294ae827c4f92842b5258dafaf17a5
Evgenii Butorin <evgeniibutorin@mail.ru>
2021-07-05 16:46:37 +0500

    product page is working

--- src/main/java/com/example/officeProductSelector/controller/MainController.java
+++ src/main/java/com/example/officeProductSelector/controller/MainController.java
@@ -1,4 +1,14 @@
 package com.example.officeProductSelector.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/product_page")
+    public String getStudentStartedPage() {
+        return "product_page";
+    }
+
 }
